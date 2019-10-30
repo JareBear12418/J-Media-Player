a26cbc6f38568011a9bfda2e7ee7620a1582af1d
JareBear <jaredgrozz@gmail.com>
2019-10-30 11:22:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,145 @@
-#pip install playsound
-from playsound import playsound
 # pip install pyqt5
-from PyQt5.QtGui import QIcon, QPalette, QColor
-from PyQt5.QtCore import pyqtSlot, Qt, pyqtSignal
+from PyQt5.QtCore import QDir, Qt, QUrl
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtWidgets, QtGui, sip
+from PyQt5.QtGui import *
 import sys
-# playsound('Music Player/test_audio.mp3')
-print('its working')
-title = 'CNC Bit Config'
+# pip install qdarkgraystyle
+import qdarkgraystyle
+
+title = 'J-Media Player'
 version = 'v0.01'
-class App(QMainWindow):
+class VideoWindow(QMainWindow):
+
     def __init__(self, parent=None):
-        super(App, self).__init__(parent)
-        self.title = title
-        self.version = version
-        self.width = canvas_width
-        self.height = canvas_height
-        self.setWindowFlags(QtCore.Qt.X11BypassWindowManagerHint)
-        self.initUI()
+        super(VideoWindow, self).__init__(parent)
+        self.setWindowTitle(title + ' ' + version) 
+        self.setWindowFlags(Qt.X11BypassWindowManagerHint)
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        videoWidget = QVideoWidget()
+
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+
+        self.errorLabel = QLabel()
+        self.errorLabel.setSizePolicy(QSizePolicy.Preferred,
+                QSizePolicy.Maximum)
+
+        # Create new action
+        openAction = QAction(QIcon('open.png'), '&Open', self)        
+        openAction.setShortcut('Ctrl+O')
+        openAction.setStatusTip('Open movie')
+        openAction.triggered.connect(self.openFile)
+
+        # Create exit action
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)        
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.exitCall)
+
+        # Create menu bar and add action
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('&File')
+        #fileMenu.addAction(newAction)
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(exitAction)
+
+        # Create a widget for window contents
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+
+        # Create layouts to place inside widget
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(0, 0, 0, 0)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.positionSlider)
+
+        layout = QVBoxLayout()
+        layout.addWidget(videoWidget)
+        layout.addLayout(controlLayout)
+        layout.addWidget(self.errorLabel)
+
+        # Set widget to contain window contents
+        wid.setLayout(layout)
+
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
         
-    def initUI(self):
-        label = QtGui.QLabel('My Label')
-        line_edit = QtGui.QLineEdit()
+        
+        app.setStyleSheet(qdarkgraystyle.load_stylesheet())
+        # Force the style to be the same on all OSs:
+        # app.setStyle("Fusion")
 
-        form_layout = QtGui.QFormLayout()
-        form_layout.addRow(label, line_edit)
+        # # Now use a palette to switch to dark colors:
+        # palette = QPalette()
+        # palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        # palette.setColor(QPalette.WindowText, Qt.white)
+        # palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        # palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ToolTipBase, Qt.white)
+        # palette.setColor(QPalette.ToolTipText, Qt.white)
+        # palette.setColor(QPalette.Text, Qt.white)
+        # palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ButtonText, Qt.white)
+        # palette.setColor(QPalette.BrightText, Qt.red)
+        # palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        # palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        # palette.setColor(QPalette.HighlightedText, Qt.black)
+        # app.setPalette(palette)
 
-        close_button = QtGui.QPushButton('Close')
-        execute_button = QtGui.QPushButton('Execute')
+    def openFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
+                QDir.homePath() + "/Videos", "Media (*.webm *.mp4 *.ts *.avi *.mpeg *.mpg *.mkv *.VOB *.m4v *.3gp *.mp3 *.m4a *.wav *.ogg *.flac *.m3u *.m3u8)")
 
-        button_layout = QtGui.QHBoxLayout()
-        button_layout.addWidget(close_button)
-        button_layout.addWidget(execute_button)
+        if fileName != '':
+            self.mediaPlayer.setMedia(
+                    QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
 
-        main_layout = QtGui.QVBoxLayout()
-        main_layout.addLayout(form_layout)
-        main_layout.addStretch()
-        main_layout.addLayout(button_layout)
+    def exitCall(self):
+        sys.exit(app.exec_())
 
-        central_widget = QtGui.QWidget()
-        central_widget.setLayout(main_layout)
-        self.setCentralWidget(central_widget)
-        self.show()
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = App()
-    main_window.show()
+    player = VideoWindow()
+    player.resize(800, 400)
+    player.show()
     sys.exit(app.exec_())
