3d8a38c6b5354f18af8544cfcf75e86ff264baed
JareBear <jaredgrozz@gmail.com>
2019-10-31 06:24:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,17 @@ from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-import sys, os, getpass, shutil, subprocess
-
-# pip instal easygui
-import easygui
-# pip install qdarkgraystyle
-# import qdarkgraystyle
-
-# pip install pytube
-from pytube import YouTube
-
-# pip install youtube-dl
-import youtube_dl
-
-import imageio
+# pip install qdarkstyle / youtube-dl, imageio
+import sys, os, getpass, shutil, subprocess, qdarkstyle, youtube_dl, imageio
 
 title = 'J-Media Player'
 version = 'v0.1'
 username = getpass.getuser()
 FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
+fileLoc = 0
 
+width = 800
+height = 400
 class VideoWindow(QMainWindow):
     def __init__(self, parent=None):
         super(VideoWindow, self).__init__(parent)
@@ -97,24 +88,25 @@ class VideoWindow(QMainWindow):
         
         # app.setStyleSheet(qdarkgraystyle.load_stylesheet())
         # Force the style to be the same on all OSs:
-        app.setStyle("Fusion")
+        # app.setStyle("Fusion")
 
-        # Now use a palette to switch to dark colors:
-        palette = QPalette()
-        palette.setColor(QPalette.Window, QColor(53, 53, 53))
-        palette.setColor(QPalette.WindowText, Qt.white)
-        palette.setColor(QPalette.Base, QColor(25, 25, 25))
-        palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
-        palette.setColor(QPalette.ToolTipBase, Qt.white)
-        palette.setColor(QPalette.ToolTipText, Qt.white)
-        palette.setColor(QPalette.Text, Qt.white)
-        palette.setColor(QPalette.Button, QColor(53, 53, 53))
-        palette.setColor(QPalette.ButtonText, Qt.white)
-        palette.setColor(QPalette.BrightText, Qt.red)
-        palette.setColor(QPalette.Link, QColor(42, 130, 218))
-        palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-        palette.setColor(QPalette.HighlightedText, Qt.black)
-        app.setPalette(palette)
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        # # Now use a palette to switch to dark colors:
+        # palette = QPalette()
+        # palette.setColor(QPalette.Window, QColor(53, 53, 53))
+        # palette.setColor(QPalette.WindowText, Qt.white)
+        # palette.setColor(QPalette.Base, QColor(25, 25, 25))
+        # palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ToolTipBase, Qt.white)
+        # palette.setColor(QPalette.ToolTipText, Qt.white)
+        # palette.setColor(QPalette.Text, Qt.white)
+        # palette.setColor(QPalette.Button, QColor(53, 53, 53))
+        # palette.setColor(QPalette.ButtonText, Qt.white)
+        # palette.setColor(QPalette.BrightText, Qt.red)
+        # palette.setColor(QPalette.Link, QColor(42, 130, 218))
+        # palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+        # palette.setColor(QPalette.HighlightedText, Qt.black)
+        # app.setPalette(palette)
 
     def openFile(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie",
@@ -166,35 +158,53 @@ class VideoWindow(QMainWindow):
     def youtubeDownloadPopup(self):
         # self.close()
         self.youtube_download_popup = youtube_download('Download Youtube Video')
-        self.youtube_download_popup.setFixedSize(300, 310)
+        self.youtube_download_popup.setFixedSize(300, 120)
         self.youtube_download_popup.setWindowTitle('Download Youtube Video')
         # self.youtube_download_popup.setWindowIcon(QtGui.QIcon('add.png'))
         self.youtube_download_popup.show()
 
-class youtube_download(QDialog):
+class youtube_download(QMainWindow):
     def __init__(self, name):
         super().__init__()
+        
         self.title = name
+        # RADIO BUTTON START
+        self.radAudio = QRadioButton(self)
+        self.radAudio.setText('Audio')
+        self.radAudio.move(10,80)
+        # RADIO BUTTON END
         # TEXT BOX START
         self.txtURL = QLineEdit(self)
-        self.txtURL.move(10,10)
-        self.txtURL.resize(250,30)
+        self.txtURL.move(10,40)
+        self.txtURL.resize(280,30)
         # TEXT BOX END
         # LABEL START
         self.lblTitle = QLabel(self)
-        self.lblTitle.move(10,100)
-        self.lblTitle.resize(250,30)
-        self.lblTitle.setText("Title: ")
+        self.lblTitle.move(10,0)
+        self.lblTitle.resize(250,20)
+        self.lblTitle.setText("")
+        
+        
+        self.lblURL = QLabel(self)
+        self.lblURL.move(10,20)
+        self.lblURL.resize(250,20)
+        self.lblURL.setText("URL: ")
+        
+        self.lblState = QLabel(self)
+        self.lblState.move(210,85)
+        self.lblState.setText("")
         # LABEL END
         # BUTTON START
         self.btnDownload = QPushButton(self)
         self.btnDownload.setText('Download')
-        self.btnDownload.move(10,60)
+        self.btnDownload.move(300 / 3,80)
         self.btnDownload.clicked.connect(self.downloadYoutube)
         # BUTTON END
     def downloadYoutube(self):
+        self.changeText()
         try:
-            directory = 'C:/Users/{}/Videos/J-Media Player Downloads'.format(username)
+            self.lblState.setText('Downloading...')
+            directory = 'C:/Users/{}/Videos/J-Tube Downloads'.format(username)
             if not os.path.exists(directory):
                 os.makedirs(directory)
                 
@@ -202,26 +212,55 @@ class youtube_download(QDialog):
             if 'https://www.youtube.com/watch?' not in url:
                 buttonReply = QMessageBox.critical(self, 'Error! :(', "{} is an invalid URL".format(url), QMessageBox.Ok, QMessageBox.Ok)
                 return
-            ydl_opts = {}
+            if self.radAudio.isChecked() == True:
+                ydl_opts = {
+                    'format': 'bestaudio/best',
+                    'extractaudio': True,
+                    'audioformat': "mp3"
+                }
+            else:
+                ydl_opts = {
+                    'format': 'bestaudio/best'
+                }
             info_dict = youtube_dl.YoutubeDL(ydl_opts).extract_info(url, download = False)
             video_id = info_dict.get("id", None)
             video_title = info_dict.get('title', None)
-            self.lblTitle.setText("Title: " + str(video_title))
+            self.lblTitle.setText(str(video_title))
             
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
-                
-            shutil.move(video_title + '-' + video_id + '.mp4', directory + '/' + video_title + '.mp4')
+            try:
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    self.lblState.setText('Downloading...')
+                    ydl.download([url])
+            except:
+                buttonReply = QMessageBox.critical(self, 'Error! :(', "Problem downloading {}".format(url), QMessageBox.Ok, QMessageBox.Ok)
+                return
+            f = os.listdir(os.getcwd())
+            t = video_title + '-' + video_id
+            for i, j in enumerate(f):
+                # print(j, i)
+                if t in j:
+                    print(j)
+                    global fileLoc
+                    fileLoc = f[i]
+                    print(fileLoc)
+                    
+            extension = os.path.splitext(fileLoc)[1]
+            shutil.move(video_title + '-' + video_id + extension, directory + '/' + video_title + extension)
+            self.lblState.setText('Finished!')
             buttonReply = QMessageBox.information(self, 'Success! :)', "Succesfully downloaded!\nDo you want to open the file directory?", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
             if buttonReply == QMessageBox.Yes:
                 self.explore(directory)
+            self.lblState.setText('')
         except Exception as e:
+            self.lblState.setText('')
+            self.lblTitle.setText("")
             buttonReply = QMessageBox.critical(self, 'Error! :(', "{}".format(e), QMessageBox.Ok, QMessageBox.Ok)
             return
+    def changeText(self):
+        self.lblState.setText('Downloading...')
     def explore(self, path):
         # explorer would choke on forward slashes
         path = os.path.normpath(path)
-
         if os.path.isdir(path):
             subprocess.run([FILEBROWSER_PATH, path])
         elif os.path.isfile(path):
@@ -229,6 +268,6 @@ class youtube_download(QDialog):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     player = VideoWindow()
-    player.resize(800, 400)
+    player.resize(width, height)
     player.show()
     sys.exit(app.exec_())
